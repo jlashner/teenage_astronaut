ae535c95565ae284cfb82d5460672f6e627cec29
jlashner <jlashner@gmail.com>
2015-08-04 22:05:43 -0400

    Initial Commit

---
+++ Teenage_Astronaut/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Teenage_Astronaut
+//
+//  Created by Jacob Lashner on 8/4/15.
+//  Copyright (c) 2015 Teenage_Astronaut. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
